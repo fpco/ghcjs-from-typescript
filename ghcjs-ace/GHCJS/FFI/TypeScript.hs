e7185709898aa1b713c010f2d5026c63927bec49
Michael Sloan <mgsloan@gmail.com>
2015-09-01 14:59:29 -0700

    Remove misc old files

--- ghcjs-ace/GHCJS/FFI/TypeScript.hs
+++
@@ -1,5 +0,0 @@
-module GHCJS.FFI.TypeScript where
-import GHCJS.Types (JSRef)
-
-data Function_
-type Function = JSRef Function_
--- ghcjs-ace/JavaScript/AceAjax/Raw/Tokenizer.hs
+++
@@ -1,9 +0,0 @@
-module JavaScript.AceAjax.Raw.Tokenizer where
-import qualified GHCJS.Types as GHCJS
-import qualified GHCJS.Marshal as GHCJS
-import qualified Data.Typeable
-import GHCJS.FFI.TypeScript
-import GHCJS.DOM.Types (HTMLElement)
-import JavaScript.AceAjax.Raw.Types
-
-foreign import javascript "$1.getLineTokens()" getLineTokens :: (Tokenizer) -> IO (GHCJS.JSRef obj0)
